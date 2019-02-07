c25ca399027e9bf49a3ff3d75b259039a27e25f7
Sidonie <sgastard@enssat.fr>
2019-02-07 10:40:06 +0100

    Liste de chapitres s'affiche

--- angular-road66/src/app/app.module.ts
+++ angular-road66/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -7,6 +7,8 @@ import { AppComponent } from './app.component';
 import { VideoComponent } from './video/video.component';
 import { ChapterListComponent } from './chapter/chapter-list/chapter-list.component';
 import { ChapterItemComponent } from './chapter/chapter-item/chapter-item.component';
+import { MatMenuModule } from '@angular/material/menu';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,9 @@ import { ChapterItemComponent } from './chapter/chapter-item/chapter-item.compon
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    MatMenuModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
