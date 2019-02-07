35b1f82d3261418d8f3342cba87a2ffc9ae3ef09
Unknown <simon.bonnaud@gmail.com>
2019-02-07 12:00:59 +0100

    .

--- angular-road66/src/app/app.component.spec.ts
+++ angular-road66/src/app/app.component.spec.ts
@@ -3,6 +3,8 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { VideoComponent } from "./video/video.component";
 
+var fixture;
+
 describe('AppComponent', () => {
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
@@ -14,22 +16,21 @@ describe('AppComponent', () => {
 				VideoComponent
 			],
 		}).compileComponents();
+
+		fixture = TestBed.createComponent(AppComponent);
 	}));
 
 	it('should create the app', () => {
-		const fixture = TestBed.createComponent(AppComponent);
 		const app = fixture.debugElement.componentInstance;
 		expect(app).toBeTruthy();
 	});
 
 	it(`should have as title 'angular-road66'`, () => {
-		const fixture = TestBed.createComponent(AppComponent);
 		const app = fixture.debugElement.componentInstance;
 		expect(app.title).toEqual('angular-road66');
 	});
 
 	it('should render title in a h1 tag', () => {
-		const fixture = TestBed.createComponent(AppComponent);
 		fixture.detectChanges();
 		const compiled = fixture.debugElement.nativeElement;
 		expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-road66!');
